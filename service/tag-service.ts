461ec5e5a2a2b4cd9a2c5191e6ef0363c9331bcd
snov <s-n-o-v@yandex.ru>
2025-05-21 00:35:09 +0300

    chore: tag service

--- service/tag-service.ts
+++ service/tag-service.ts
@@ -7,38 +7,3 @@ export class TagService extends BaseService<ITag, { name: string }> {
     super('/api/tag');
   }
 }
-// const TagService = () => {
-//   const baseUrl: string = "/api/tag";
-
-//   const list = async () => { // no reload for list
-//     const { data, error } = await useFetch(`/api/tag/list`);
-//     if (error.value) {
-//       throw new Error(error.value?.data?.message || 'Fetch error');
-//     }
-
-//     return data.value;
-//   };
-
-//   const create = async (name: string) => {
-//     const { data: response } = await useFetch("/api/tag/new", { method: "POST", body: { name} });
-//     return response?.value;
-//   };
-
-//   const update = async (body: ITag) => {
-//     const { data: response } = await useFetch(`/api/tag/update`, { method: "POST", body });
-//     return response?.value;
-//   };
-
-//   const remove = async (id: number) => {
-//     return await useFetch(`/api/tag/remove`, { method: "POST", body: { id } });
-//   };
-
-//   return {
-//     list,
-//     create,
-//     update,
-//     remove,
-//   };
-// };
-
-// export default TagService;
